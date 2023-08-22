0812047e22fc3d06c7064a3d27c093d1c8748df0
adityapalve <70425922+adityapalve@users.noreply.github.com>
2023-08-22 22:50:08 +0100

    more cleaning

--- app/crud.py
+++ app/crud.py
@@ -3,8 +3,8 @@ from sqlalchemy import Session
 from . import keygen, models, schemas
 
 def create_db_url(db: Session, url: schemas.BaseURL) -> models.URL:
-    key = keygen.create_random_key()
-    secret_key = keygen.create_random_key(length=8)
+    key = keygen.create_unique_random_key(db)
+    secret_key = f"{key}_{keygen.create_random_key(length=8)}"
     db_url = models.URL(
         target_url=url.target_url, key=key, secret_key=secret_key
     )
@@ -12,4 +12,7 @@ def create_db_url(db: Session, url: schemas.BaseURL) -> models.URL:
     db.commit()
     db.refresh(db_url)
     return db_url
-  
+
+# This function either returns the URL object or returns a None object.
+def get_db_url_by_key(db: Session, url_key: str) -> models.URL:
+    return db.query(models.URL).filter(models.URL.key == url_key, models.URL.is_active).first()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 import validators
-from . import schemas, models
-import secrets
+from . import  crud, schemas, models
 from sqlalchemy import Session
 from .database import SessionLocal, engine
 
@@ -38,30 +37,18 @@ def raise_bad_request(message: str):
 
 
 @app.post("/url", response_model=schemas.URLInfo)
-def create_url(url: schemas.BaseURL):
+def create_url(url: schemas.BaseURL, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
-        raise_bad_request(message="The provided URL is invalid")
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    key = "".join(secrets.choice(chars) for i in range(5))
-    secret_key = "".join(secrets.choice(chars) for i in range(8))
-    db_url = models.URL(
-        target_url = url.target_url, key=key, secret_key=secret_key
-    )
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
-    db_url.url = key
-    db_url.admin_url = secret_key
+            raise_bad_request(message="The provided URL is invalid")
+        
+    db_url = crud.create_db_url(db=db, url=url)
+    db_url.url = db_url.key
+    db_url.admin_url = db_url.secret_key
     return db_url
 
 @app.get("/{url_key}")
-def forward_to_target_url(
-    url_key:str,
-    request: Request,
-    db: Session = Depends(get_db)
-):
-    db_url = db.query(models.URL).filter(models.URL.key == url_key).first()
-    if not db_url:
+def forward_to_target_url(url_key:str, request: Request, db: Session = Depends(get_db)):
+    if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
         return RedirectResponse(db_url.target_url)
     else:
         raise_not_found(request)
